292092bb7eb4821684485a350fea854b23dab84b
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-25 12:16:31 +1000

    Вложенная группа конфигурации БД (#28)

--- app/configs/__init__.py
+++ app/configs/__init__.py
@@ -1,8 +1,12 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from .database import DatabaseConfiguration
 
 
 class Projectonfiguration(BaseSettings):
     model_config = SettingsConfigDict(env_prefix="BOOKSHELF_")
+
+    # * Вложенные группы настроек
+    database: DatabaseConfiguration = DatabaseConfiguration()
 
     # * Опциональные переменные
     DEBUG_MODE: bool = True
