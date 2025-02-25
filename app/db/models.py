92dec6df7a1eb1441199430bb27fbe7d0e2df8d3
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-25 17:33:31 +1000

    Модель Publishers и миграции (#20)

--- app/db/models.py
+++ app/db/models.py
@@ -1,1 +1,19 @@
+import uuid
+
+from sqlalchemy import Column, Index, String, Text
+from sqlalchemy.dialects.postgresql import UUID
+
 from .engine import BaseORM
+
+
+class Publisher(BaseORM):
+    __tablename__ = "publishers"
+
+    code = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(Text, nullable=False)
+    city = Column(String(60), default=None)
+
+    __table_args__ = (
+        Index("publisher_code_idx", code, postgresql_using="hash"),
+        Index("publishers_name_idx", name, postgresql_using="hash"),
+    )
