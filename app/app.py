4dde572ab2c3bc1fbd22ed82c713b3d556818aa6
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-25 13:27:49 +1000

    Закрытие подключения в Lifespan

--- app/app.py
+++ app/app.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from routers import v1_router
 
+from db import disconnect_db
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -11,6 +13,7 @@ async def lifespan(app: FastAPI):
     yield
 
     # После выключения
+    await disconnect_db()
 
 
 app = FastAPI(lifespan=lifespan)
