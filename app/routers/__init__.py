69fd3395a4ebc30fa7dfe3171d2b8652545edb70
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-26 19:48:12 +1000

    Рефактор импортов

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
+
+from .authors import router as authors_router
+from .books import router as books_router
 from .health import router as health_router
 from .publishers import router as publishers_router
-from .authors import router as authors_router
 from .readers import router as readers_router
 
 api_router = APIRouter(prefix="/api")
@@ -10,5 +12,6 @@ api_router.include_router(health_router)
 api_router.include_router(publishers_router)
 api_router.include_router(authors_router)
 api_router.include_router(readers_router)
+api_router.include_router(books_router)
 
 __all__ = ("api_router",)
