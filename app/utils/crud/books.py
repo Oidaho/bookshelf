c6e29d0483dade5bc6d769182cb0f02e19a9ceda
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-27 00:16:21 +1000

    Комментарии к коду (#26)

--- app/utils/crud/books.py
+++ app/utils/crud/books.py
@@ -6,14 +6,18 @@ from .authors import author
 from .base import CRUD
 from .publishers import publisher
 
+# ! Если настолько сложные взаимодействия не будут интересовать в рамках CRUD -
+# ! удалите все переопределенные методы и оставьте pass в теле класса.
+
 
 class BookCRUD(CRUD[Book]):
     async def create(self, db, obj_in):
-        # Переменные сокращены с целью предотвращения конфликтов имен
+        # * Переменные сокращены с целью предотвращения конфликтов имен
         a = await author.get(db, code=obj_in.pop("author_code"))
         p = await publisher.get(db, code=obj_in.pop("publisher_code"))
 
         try:
+            # * Создаем книгу
             db_obj = self.model(**obj_in)
             db_obj.author = a
             db_obj.publisher = p
@@ -22,6 +26,7 @@ class BookCRUD(CRUD[Book]):
             await db.commit()
             await db.refresh(db_obj)
 
+        # * Если такая книга уже существует
         except IntegrityError:
             await db.rollback()
             raise HTTPException(
@@ -29,6 +34,7 @@ class BookCRUD(CRUD[Book]):
                 detail=f"{self.model.__name__} with this data already exists.",
             )
 
+        # * Другие ошибки целосности отработают на уровне Pyndantic схем
         except Exception:
             await db.rollback()
             raise HTTPException(
@@ -41,23 +47,26 @@ class BookCRUD(CRUD[Book]):
     async def delete(self, db, code, raise_404=True):
         db_obj = await self.get(db, code, raise_404)
 
-        # Переменные сокращены с целью предотвращения конфликтов имен
+        # * Переменные сокращены с целью предотвращения конфликтов имен
         a = await db_obj.awaitable_attrs.author
         p = await db_obj.awaitable_attrs.publisher
 
         try:
+            # * Удаление книги
             await db.delete(db_obj)
             await db.commit()
 
+            # * Если у связанного автора больше нет книг
             if len(await a.awaitable_attrs.books) < 1:
                 await db.delete(a)
                 await db.commit()
 
+            # * Если у связанного издательства больше нет книг
             if len(await p.awaitable_attrs.books) < 1:
                 await db.delete(p)
                 await db.commit()
 
-        # Ошибки целосности отработают на уровне Pyndantic схем
+        # * Ошибки целосности отработают на уровне Pyndantic схем
         except Exception:
             await db.rollback()
             raise HTTPException(
