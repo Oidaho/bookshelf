ccf07d970f63f3d1c1bdedda028051a770af965c
Oidaho <76991612+Oidaho@users.noreply.github.com>
2025-02-27 13:34:29 +1000

    Прототип роута /api/reports (#27)

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -3,9 +3,10 @@ from fastapi import APIRouter
 from .authors import router as authors_router
 from .books import router as books_router
 from .health import router as health_router
+from .issuances import router as issuances_router
 from .publishers import router as publishers_router
 from .readers import router as readers_router
-from .issuances import router as issuances_router
+from .reports import router as reports_router
 
 api_router = APIRouter(prefix="/api")
 
@@ -15,5 +16,6 @@ api_router.include_router(authors_router)
 api_router.include_router(readers_router)
 api_router.include_router(books_router)
 api_router.include_router(issuances_router)
+api_router.include_router(reports_router)
 
 __all__ = ("api_router",)
